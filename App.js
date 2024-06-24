f38c2293aff1f05bfcc523bc0f5e8afa8031c0a7
debu18042022 <sujeetsahu322@gmail.com>
2024-06-24 18:20:56 +0530

    ignition our app

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
-const heading1 = React.createElement("h1",{id:"title1",},"Hello Everyone");
-const heading2 = React.createElement("h2",{id:"title2",style:{color:"red"}},"Welcome in Namastey React");
-const container = React.createElement("div",{id:"container"},[heading1,heading2]);
+import React from 'react';
+import ReactDOM from 'react-dom';
+const heading1 = React.createElement("h1", { id: "title1" }, "Hello Everyone");
+const heading2 = React.createElement(
+  "h2",
+  { id: "title2", style: { color: "red" } },
+  "Welcome in Namastey React"
+);
+const container = React.createElement("div", { id: "container" }, [
+  heading1,
+  heading2,
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(container);
